ccf5df049bca6bc73517f8c11db7c02ec54d7252
thibaultchretien <thibault.chretien021@icloud.com>
2024-11-04 20:49:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def predict():
 # Lancer l'application Flask
 if __name__ == '__main__':
     logger.info("Démarrage de l'application Flask")  # Log démarrage application
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
